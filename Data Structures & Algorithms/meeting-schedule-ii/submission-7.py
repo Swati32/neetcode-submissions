54826343ede0705961ac600807932832ee2e7bbc
Swati <swati.sisodia61@gmail.com>
2026-09-17 09:16:15 -0700

    Add: meeting-schedule-ii - submission-7

---
+++ Data Structures & Algorithms/meeting-schedule-ii/submission-7.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def minMeetingRooms(self, intervals: List[Interval]) -> int:
+        intervals.sort(key=lambda x: x.start)
+        active_rooms = []
+
+        for interval in intervals:
+            if active_rooms and active_rooms[0] <= interval.start:
+                heapq.heappop(active_rooms)
+
+            heapq.heappush(active_rooms, interval.end)
+            
+        return len(active_rooms)
